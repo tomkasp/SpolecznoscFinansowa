1df54a9511eb6307669ee0e415655f82ac6f293f
WR1EI1 <WR1EI1@192.168.0.4>
2013-06-04 13:29:45 +0200

    ui

--- src/java/test/NewMain.java
+++ src/java/test/NewMain.java
@@ -13,16 +13,16 @@ public class NewMain {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) { 
        //GeneratorPDF.generuj(1);
-        
-        
-        
-        
         KlienciDao kdao=new KlienciDao();
         List<Klienci> l = kdao.getKlientList();
         System.out.println("P: "+ l.get(0).getImie() );
+        
+        
+        //KlienciDao kdao=new KlienciDao();
+        //List<Klienci> l = kdao.getKlientList();
+        //System.out.println("P: "+ l.get(0).getImie() );
         
        // System.out.println("QQQQQQQQQQ");
         
