74aac1410ab9626e13e9546026b903ce8ea52896
Admin <Admin@192.168.0.3>
2013-06-28 15:18:49 +0200

    MadejsoN - poprawki

--- src/java/sql/dao/UzytkownikDao.java
+++ src/java/sql/dao/UzytkownikDao.java
@@ -4,57 +4,67 @@ import java.security.NoSuchAlgorithmException;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import sql.entity.Uzytkownik;
 import sql.util.HibernateUtil;
 import sql.util.Security;
 
 public class UzytkownikDao {
 
-    private String message="";
-    
-    public Boolean logowanie(String login, String haslo) {
-        
-        
-        try {
-            Session session = HibernateUtil.getSessionFactory().openSession();
+    private String message = "";
 
+    public Boolean logowanie(String login, String haslo) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
             System.out.println("probuje logowac");
             Query q = session.createQuery("FROM Uzytkownik WHERE login='" + login + "' AND haslo='" + Security.sha1(haslo) + "' ");
 
             System.out.println(q);
             if (q.list().isEmpty()) {
-                Query q2 = session.createQuery("FROM Uzytkownik WHERE login='" + login +" ");
-                message="Brak użytkownika";
-                if(q2.list().isEmpty())
-                {
-                message="bledne haslo";
+                Query q2 = session.createQuery("FROM Uzytkownik WHERE login='" + login + " ");
+                message = "Brak użytkownika";
+                if (q2.list().isEmpty()) {
+                    message = "bledne haslo";
                 }
-                
-                Query q3 = session.createQuery("FROM Uzytkownik WHERE haslo='" + Security.sha1(haslo) +" ");
-                if(q3.list().isEmpty())
-                {
-                message="bledny login";
+
+                Query q3 = session.createQuery("FROM Uzytkownik WHERE haslo='" + Security.sha1(haslo) + " ");
+                if (q3.list().isEmpty()) {
+                    message = "bledny login";
                 }
-                
+
                 return false;
             } else {
-                String message="";
-                message="logowanie przebiegli poprawnie";
+                String message = "";
+                message = "Nastąpiło poprawne logowanie do SyStemu.";
                 return true;
             }
             
-            
-        } catch (HibernateException | NoSuchAlgorithmException e) {
-            message="Błąd połączenia z bazą";
-            System.out.println("test");
-        }
-        finally {
-                System.out.println(message);
-        }
 
-        //session.close();    
+        } catch (HibernateException | NoSuchAlgorithmException e) {
+            message = "Błąd połączenia z bazą";
+            System.out.println("test");
+        } finally {
+            System.out.println(message);
+        }
+        session.close();
+            
         return false;
     }
 
+    
+    public void dodajUzytkownika(Uzytkownik user){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        
+        Uzytkownik uzytkownik = user;
+        
+        session.save(uzytkownik);
+        session.getTransaction().commit();
+        session.close();
+    
+    }
+    
+    
+    
     public String getMessage() {
         return message;
     }
@@ -62,5 +72,4 @@ public class UzytkownikDao {
     public void setMessage(String message) {
         this.message = message;
     }
-    
 }
