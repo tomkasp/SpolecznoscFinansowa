5ca80653f0acc863176f7ee752933383f86a9ae7
Gury <Gury@Gury-Komputer>
2013-07-26 11:45:36 +0200

    dodanie walidacji

--- src/main/java/com/efsf/sf/beans/FakeBean2.java
+++ src/main/java/com/efsf/sf/beans/FakeBean2.java
@@ -17,26 +17,79 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class FakeBean2 {
 
-    private ArrayList<FakeDataHolder> fdh = new ArrayList();
+    private ArrayList<FakeDataHolder2> fdh = new ArrayList();
+    private ArrayList<FakeDataHolder2> fdh2 = new ArrayList();
+    private ArrayList<FakeDataHolder2> fdh3 = new ArrayList();
+    private ArrayList<FakeDataHolder2> fdh4 = new ArrayList();
     
+    private FakeDataHolder2 selectedFakeDataHolder=new FakeDataHolder2();
     /**
      * Creates a new instance of FakeBean
      */
     
     public FakeBean2() {
-        fdh.add(new FakeDataHolder("1","00012", "05.05.2012","00123" ,"30%","hipoteka", "nie", "32-300", "45", "Analiza", "UOPO", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata"));
-        fdh.add(new FakeDataHolder("2","00432", "06.05.2012","00123" ,"20%","konsolidacyjny", "nie", "44-050", "31", "Budownictwo", "UOPO", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata" ));
-        fdh.add(new FakeDataHolder("3","00467", "07.05.2012","00123" ,"60%","hipoteka", "tak", "54-200", "66", "Emeryt", "UOPO", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata" ));
-        fdh.add(new FakeDataHolder("4","00112", "08.05.2012","00123" ,"10%","hipoteka", "nie", "44-320", "43", "Finanse", "UOPO", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata" ));
-        fdh.add(new FakeDataHolder("5","00612", "09.05.2012","00123" ,"30%","chwilówka", "nie", "21-100", "25", "Inne", "UOPO", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata" ));
-        fdh.add(new FakeDataHolder("6","02312", "10.05.2012","00123" ,"80%","hipoteka", "nie", "63-520", "23", "Edukacja", "UOPO", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata" ));
+        fdh.add(new FakeDataHolder2(false, "1","00012", "05.05.2012","00123" ,"30%","hipoteka", "nie", "32-300", "45",  "UOPO","Analiza", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata",""));
+        fdh.add(new FakeDataHolder2(false,"2","00432", "06.05.2012","00123" ,"20%","chwilówka", "nie", "44-050", "31",  "UOPO","Budownictwo", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata","Dwa razy więcej punktów" ));
+        fdh.add(new FakeDataHolder2(false,"3","00467", "07.05.2012","00123" ,"60%","hipoteka", "tak", "54-200", "66", "UOPO", "Emeryt", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata" ,""));
+        fdh.add(new FakeDataHolder2(false,"4","00112", "08.05.2012","00123" ,"10%","hipoteka", "nie", "44-320", "43",  "UOPO","Finanse", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata","" ));
+        fdh.add(new FakeDataHolder2(false,"5","00612", "09.05.2012","00123" ,"30%","chwilówka", "nie", "21-100", "25",  "UOPO","Inne", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata","" ));
+        fdh.add(new FakeDataHolder2(false,"6","02312", "10.05.2012","00123" ,"80%","hipoteka", "nie", "63-520", "23", "UOPO", "Edukacja", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata","" ));
+   
+        fdh2.add(new FakeDataHolder2(true,"3","00467", "07.05.2012","00123" ,"60%","hipoteka", "tak", "54-200", "66", "UOPO", "Emeryt", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata" ,""));
+        fdh2.add(new FakeDataHolder2(true,"4","00112", "08.05.2012","00123" ,"10%","hipoteka", "nie", "44-320", "43",  "UOPO","Finanse", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata","" ));
+        fdh2.add(new FakeDataHolder2(true,"5","00612", "09.05.2012","00123" ,"30%","chwilówka", "nie", "21-100", "25",  "UOPO","Inne", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata","" ));
+       
+        fdh3.add(new FakeDataHolder2(true,"3","00467", "07.05.2012","00123" ,"60%","hipoteka", "tak", "54-200", "66", "UOPO", "Emeryt", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Popros o ocene", "zalegle", "rata" ,""));
+        
+        
+        fdh4.add(new FakeDataHolder2(true,"3","00467", "07.05.2012","00123" ,"60%","hipoteka", "tak", "54-200", "66", "UOPO", "Emeryt", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata" ,""));
+        fdh4.add(new FakeDataHolder2(true,"4","00112", "08.05.2012","00123" ,"10%","hipoteka", "nie", "44-320", "43",  "UOPO","Finanse", "7/10", "6", "25h", "12", "analiza oferty", "nie", "20.05.2012", "200 000", "Oceń", "zalegle", "rata","" ));
+        
+        
     }
 
-    public ArrayList<FakeDataHolder> getFdh() {
+    public ArrayList<FakeDataHolder2> getFdh() {
         return fdh;
     }
 
-    public void setFdh(ArrayList<FakeDataHolder> fdh) {
+    public void setFdh(ArrayList<FakeDataHolder2> fdh) {
         this.fdh = fdh;
     }
+
+    public ArrayList<FakeDataHolder2> getFdh2() {
+        return fdh2;
+    }
+
+    public void setFdh2(ArrayList<FakeDataHolder2> fdh2) {
+        this.fdh2 = fdh2;
+    }
+
+    public ArrayList<FakeDataHolder2> getFdh3() {
+        return fdh3;
+    }
+
+    public void setFdh3(ArrayList<FakeDataHolder2> fdh3) {
+        this.fdh3 = fdh3;
+    }
+
+    public ArrayList<FakeDataHolder2> getFdh4() {
+        return fdh4;
+    }
+
+    public void setFdh4(ArrayList<FakeDataHolder2> fdh4) {
+        this.fdh4 = fdh4;
+    }
+    
+    
+
+    public FakeDataHolder2 getSelectedFakeDataHolder() {
+        return selectedFakeDataHolder;
+    }
+
+    public void setSelectedFakeDataHolder(FakeDataHolder2 selectedFakeDataHolder) {
+        this.selectedFakeDataHolder = selectedFakeDataHolder;
+    }
+    
+    
+    
 }
---
+++ src/main/java/com/efsf/sf/beans/FakeDataHolder2.java
@@ -0,0 +1,279 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.efsf.sf.beans;
+
+/**
+ *
+ * @author XaI
+ */
+public class FakeDataHolder2 
+{
+
+    public FakeDataHolder2() {
+    }
+
+    private boolean selected;
+    private String lp;
+    private String numerKlienta;
+    private String dataDodania; 
+    private String numerSprawy;
+    private String postęp;
+    private String rodzajKredytu;
+    private String bik;
+    private String kodPocztowy;
+    private String wiek;
+    private String typZatrudnienia;
+    private String branża;
+    private String trudnościKredytu;
+    private String ilośćDoradców; 
+    private String koniecAukcji;
+    private String ilośćWyświetleń;
+    private String status;
+    private String umowa;
+    private String dataZakończenia;
+    private String kwota;
+    private String ocenaDoradcy;
+
+
+    private String zalegle;
+    private String rata;
+    
+    private String bonus;
+
+    public FakeDataHolder2(boolean selected, String lp, String numerKlienta, String dataDodania, String numerSprawy, String postęp, String rodzajKredytu, String bik, String kodPocztowy, String wiek, String typZatrudnienia, String branża, String trudnościKredytu, String ilośćDoradców, String koniecAukcji, String ilośćWyświetleń, String status, String umowa, String dataZakończenia, String kwota, String ocenaDoradcy, String zalegle, String rata, String bonus) {
+        this.selected = selected;
+        this.lp = lp;
+        this.numerKlienta = numerKlienta;
+        this.dataDodania = dataDodania;
+        this.numerSprawy = numerSprawy;
+        this.postęp = postęp;
+        this.rodzajKredytu = rodzajKredytu;
+        this.bik = bik;
+        this.kodPocztowy = kodPocztowy;
+        this.wiek = wiek;
+        this.typZatrudnienia = typZatrudnienia;
+        this.branża = branża;
+        this.trudnościKredytu = trudnościKredytu;
+        this.ilośćDoradców = ilośćDoradców;
+        this.koniecAukcji = koniecAukcji;
+        this.ilośćWyświetleń = ilośćWyświetleń;
+        this.status = status;
+        this.umowa = umowa;
+        this.dataZakończenia = dataZakończenia;
+        this.kwota = kwota;
+        this.ocenaDoradcy = ocenaDoradcy;
+        this.zalegle = zalegle;
+        this.rata = rata;
+        this.bonus = bonus;
+    }
+    
+    
+   
+    
+        public FakeDataHolder2(String lp, String rodzajKredytu, String dataZakończenia, String kwota, String zalegle, String rata) {
+            this.lp = lp;
+            this.rodzajKredytu = rodzajKredytu;
+            this.dataZakończenia = dataZakończenia;
+            this.kwota = kwota;
+            this.zalegle = zalegle;
+            this.rata = rata;
+        }
+
+    public String getLp() {
+        return lp;
+    }
+
+    public void setLp(String lp) {
+        this.lp = lp;
+    }
+
+    public String getNumerKlienta() {
+        return numerKlienta;
+    }
+
+    public void setNumerKlienta(String numerKlienta) {
+        this.numerKlienta = numerKlienta;
+    }
+
+    public String getDataDodania() {
+        return dataDodania;
+    }
+
+    public void setDataDodania(String dataDodania) {
+        this.dataDodania = dataDodania;
+    }
+
+    public String getNumerSprawy() {
+        return numerSprawy;
+    }
+
+    public void setNumerSprawy(String numerSprawy) {
+        this.numerSprawy = numerSprawy;
+    }
+
+    public String getPostęp() {
+        return postęp;
+    }
+
+    public void setPostęp(String postęp) {
+        this.postęp = postęp;
+    }
+
+    public String getRodzajKredytu() {
+        return rodzajKredytu;
+    }
+
+    public void setRodzajKredytu(String rodzajKredytu) {
+        this.rodzajKredytu = rodzajKredytu;
+    }
+
+    public String getBik() {
+        return bik;
+    }
+
+    public void setBik(String bik) {
+        this.bik = bik;
+    }
+
+    public String getKodPocztowy() {
+        return kodPocztowy;
+    }
+
+    public void setKodPocztowy(String kodPocztowy) {
+        this.kodPocztowy = kodPocztowy;
+    }
+
+    public String getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(String wiek) {
+        this.wiek = wiek;
+    }
+
+    public String getTypZatrudnienia() {
+        return typZatrudnienia;
+    }
+
+    public void setTypZatrudnienia(String typZatrudnienia) {
+        this.typZatrudnienia = typZatrudnienia;
+    }
+
+    public String getBranża() {
+        return branża;
+    }
+
+    public void setBranża(String branża) {
+        this.branża = branża;
+    }
+
+    public String getTrudnościKredytu() {
+        return trudnościKredytu;
+    }
+
+    public void setTrudnościKredytu(String trudnościKredytu) {
+        this.trudnościKredytu = trudnościKredytu;
+    }
+
+    public String getIlośćDoradców() {
+        return ilośćDoradców;
+    }
+
+    public void setIlośćDoradców(String ilośćDoradców) {
+        this.ilośćDoradców = ilośćDoradców;
+    }
+
+    public String getKoniecAukcji() {
+        return koniecAukcji;
+    }
+
+    public void setKoniecAukcji(String koniecAukcji) {
+        this.koniecAukcji = koniecAukcji;
+    }
+
+    public String getIlośćWyświetleń() {
+        return ilośćWyświetleń;
+    }
+
+    public void setIlośćWyświetleń(String ilośćWyświetleń) {
+        this.ilośćWyświetleń = ilośćWyświetleń;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUmowa() {
+        return umowa;
+    }
+
+    public void setUmowa(String umowa) {
+        this.umowa = umowa;
+    }
+
+    public String getDataZakończenia() {
+        return dataZakończenia;
+    }
+
+    public void setDataZakończenia(String dataZakończenia) {
+        this.dataZakończenia = dataZakończenia;
+    }
+
+    public String getKwota() {
+        return kwota;
+    }
+
+    public void setKwota(String kwota) {
+        this.kwota = kwota;
+    }
+
+    public String getOcenaDoradcy() {
+        return ocenaDoradcy;
+    }
+
+    public void setOcenaDoradcy(String ocenaDoradcy) {
+        this.ocenaDoradcy = ocenaDoradcy;
+    }
+    
+        public String getZalegle() {
+        return zalegle;
+    }
+
+    public void setZalegle(String zalegle) {
+        this.zalegle = zalegle;
+    }
+
+    public String getRata() {
+        return rata;
+    }
+
+    public void setRata(String rata) {
+        this.rata = rata;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public String getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(String bonus) {
+        this.bonus = bonus;
+    }
+    
+    
+   
+}
+
