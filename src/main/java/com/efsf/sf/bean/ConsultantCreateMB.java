3716e133038d3f52c7fac8ab7b92e051d06849a8
WR1EI1 <WR1EI1@192.168.0.4>
2013-07-31 09:31:03 +0200

    uii

--- src/main/java/com/efsf/sf/bean/ConsultantCreateMB.java
+++ src/main/java/com/efsf/sf/bean/ConsultantCreateMB.java
@@ -1,11 +1,15 @@
 package com.efsf.sf.bean;
 
 import com.efsf.sf.sql.dao.ConsultantDAO;
+import com.efsf.sf.sql.dao.SubscriptionDAO;
+import com.efsf.sf.sql.dao.SubscriptionTypeDAO;
 import com.efsf.sf.sql.dao.UserDAO;
 import com.efsf.sf.sql.dao.WorkingPlaceDAO;
 import com.efsf.sf.sql.entity.Address;
 import com.efsf.sf.sql.entity.Consultant;
 import com.efsf.sf.sql.entity.Institution;
+import com.efsf.sf.sql.entity.Subscription;
+import com.efsf.sf.sql.entity.SubscriptionType;
 import com.efsf.sf.sql.entity.User;
 import com.efsf.sf.sql.entity.WorkingPlace;
 import java.io.Serializable;
@@ -14,67 +18,84 @@ import java.util.Date;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-
 /**
  * @author WR1EI1
  */
 @ManagedBean
 @SessionScoped
 public class ConsultantCreateMB implements Serializable{
-
+    
     //ConsultantCreateAccount
     private User user=new User();
     private Consultant consultant=new Consultant();
     private String confirmPassword=new String();
+    
     //ConsultantFillAccountData
+    private Address mainAddress=new Address();
+    private Address invoiceAddress=new Address();
     
-    private Address mainAddress;
-    private Address invoiceAddress;
+    private List<Institution> selectedBankList=new ArrayList<>();
+    private List<Institution> selectedInstitutionList=new ArrayList<>();
     
-    private List<Institution> selectedInstitution1,selectedInstitution2,selectedInstitution3,selectedInstitution4;
+    private SubscriptionType subscriptionType=new SubscriptionType();
     
+    private Boolean policy=false;
+    private Boolean policy2=false;
+    
+    private Integer workingPlaceId=0;
     
     public ConsultantCreateMB() {
     }
     
     public String savePart1() {
-        user.setLogin( String.valueOf( new Date().getTime() ) );
         UserDAO udao=new UserDAO();
+        
+        //SET USER TYPE:
+        user.setType(1);
+        
+        user.setLogin( String.valueOf( new Date().getTime() ) );
         udao.save(user);
         user.setLogin( user.getIdUser().toString() );
         udao.update(user);
         
-        WorkingPlaceDAO wpdao=new WorkingPlaceDAO();
-        WorkingPlace wp=(WorkingPlace) wpdao.regionList().get(0);
         
+        WorkingPlaceDAO wpdao=new WorkingPlaceDAO();
+        WorkingPlace wp=(WorkingPlace) wpdao.workingPlaceList().get(0);
+        if(wp!=null)
+        {
+        consultant.setWorkingPlace(wp);
+        }
         ConsultantDAO cdao=new ConsultantDAO();
         consultant.setUser(user);
-        consultant.setWorkingPlace(wp);
         cdao.save(consultant);
+        
+        Subscription trialSubscription=new Subscription();
+        trialSubscription.setConsultant(consultant);
+        SubscriptionTypeDAO stdao=new SubscriptionTypeDAO();
+        SubscriptionType st=stdao.getSubscriptionType(4);
+        if(st!=null){
+        trialSubscription.setSubscriptionType(st); 
+        //PAMIETAJ O DODANIU DATY W PRZYSZŁOŚCI
+        SubscriptionDAO sdao=new SubscriptionDAO();
+        sdao.save(trialSubscription);
+        }
         
         return "/consultant/consultantFillAccountData?faces-redirect=true";   
     }
     
     public String savePart2() {
-        user.setLogin("");
-        UserDAO udao=new UserDAO();
-        udao.save(user);
         
-        WorkingPlaceDAO wpdao=new WorkingPlaceDAO();
-        WorkingPlace wp=(WorkingPlace) wpdao.regionList().get(0);
+        //UserDAO udao=new UserDAO();
+        //udao.update(user);
         
         ConsultantDAO cdao=new ConsultantDAO();
-        consultant.setUser(user);
-        consultant.setWorkingPlace(wp);
-        cdao.save(consultant);
+        cdao.update(consultant);
         
-        return "/consultant/consultantFillAccountData";   
+        return "/consultant/consultantMainPage?faces-redirect=true";   
     }
     
-    public String show(){  
-        
-        System.out.println( "Zaznaczono:"+ selectedInstitution1.size() );   
-        
+    public String show(){       
+        System.out.println( "Zaznaczono:"+ selectedBankList.size() );   
         return "/consultant/consultantFillAccountData?faces-redirect=true";   
     }
     
@@ -118,44 +139,54 @@ public class ConsultantCreateMB implements Serializable{
         this.invoiceAddress = invoiceAddress;
     }
 
-    public List<Institution> getSelectedInstitution1() {
-        return selectedInstitution1;
+    public List<Institution> getSelectedBankList() {
+        return selectedBankList;
     }
 
-    public void setSelectedInstitution1(List<Institution> selectedInstitution1) {
-        this.selectedInstitution1 = selectedInstitution1;
+    public void setSelectedBankList(List<Institution> selectedBankList) {
+        this.selectedBankList = selectedBankList;
     }
 
-    public List<Institution> getSelectedInstitution2() {
-        return selectedInstitution2;
+    public List<Institution> getSelectedInstitutionList() {
+        return selectedInstitutionList;
     }
 
-    public void setSelectedInstitution2(List<Institution> selectedInstitution2) {
-        this.selectedInstitution2 = selectedInstitution2;
+    public void setSelectedInstitutionList(List<Institution> selectedInstitutionList) {
+        this.selectedInstitutionList = selectedInstitutionList;
     }
 
-    public List<Institution> getSelectedInstitution3() {
-        return selectedInstitution3;
+    public SubscriptionType getSubscriptionType() {
+        return subscriptionType;
     }
 
-    public void setSelectedInstitution3(List<Institution> selectedInstitution3) {
-        this.selectedInstitution3 = selectedInstitution3;
+    public void setSubscriptionType(SubscriptionType subscriptionType) {
+        this.subscriptionType = subscriptionType;
     }
 
-    public List<Institution> getSelectedInstitution4() {
-        return selectedInstitution4;
+    public Boolean getPolicy() {
+        return policy;
     }
 
-    public void setSelectedInstitution4(List<Institution> selectedInstitution4) {
-        this.selectedInstitution4 = selectedInstitution4;
+    public void setPolicy(Boolean policy) {
+        this.policy = policy;
+    }
+
+    public Boolean getPolicy2() {
+        return policy2;
+    }
+
+    public void setPolicy2(Boolean policy2) {
+        this.policy2 = policy2;
+    }
+
+    public Integer getWorkingPlaceId() {
+        return workingPlaceId;
+    }
+
+    public void setWorkingPlaceId(Integer workingPlaceId) {
+        this.workingPlaceId = workingPlaceId;
     }
 
     
-
-   
-
-   
-    
-    
-    
+  
 }
