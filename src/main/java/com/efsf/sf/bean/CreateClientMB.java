2897e85c075818d53842540a2fb27c81cd035712
Doradca <Doradca@192.168.0.38>
2013-07-31 09:52:45 +0200

    remote

--- src/main/java/com/efsf/sf/bean/CreateClientMB.java
+++ src/main/java/com/efsf/sf/bean/CreateClientMB.java
@@ -4,19 +4,29 @@
  */
 package com.efsf.sf.bean;
 
+import com.efsf.sf.collection.IncomeData;
 import com.efsf.sf.sql.dao.ClientDAO;
 import com.efsf.sf.sql.dao.EducationDAO;
 import com.efsf.sf.sql.dao.MaritalStatusDAO;
 import com.efsf.sf.sql.dao.UserDAO;
+import com.efsf.sf.sql.entity.Address;
+import com.efsf.sf.sql.entity.Branch;
 import com.efsf.sf.sql.entity.Client;
+import com.efsf.sf.sql.entity.EmploymentType;
+import com.efsf.sf.sql.entity.Income;
+import com.efsf.sf.sql.entity.IncomeBuisnessActivity;
+import com.efsf.sf.sql.entity.Region;
 import com.efsf.sf.sql.entity.User;
 import com.efsf.sf.util.Security;
 import java.io.Serializable;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
@@ -27,17 +37,20 @@ import javax.faces.validator.ValidatorException;
  * @author XaI
  */
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class CreateClientMB implements Serializable
 {
     private static final long serialVersionUID = 1L;
     
-
-    
     @ManagedProperty(value="#{loginMB}")
     private LoginMB loginMB;
     
+    @ManagedProperty(value="#{dictionaryMB}")
+    private DictionaryMB dictionaryMB;
+    
     User user; 
+    
+    //General information
     
     private int loginNumber;
     private String  login;
@@ -45,6 +58,43 @@ public class CreateClientMB implements Serializable
     private String  password;
     private String  passwordCheck;
     private String  email; 
+    private String surname; 
+    private String familyName;
+    private Date birthDate;
+    private int maritalStatus;
+    private int education; 
+    private Boolean sex;
+    private String pesel;
+    private int birthPlace;
+    
+    //Income
+    
+    private ArrayList<IncomeData> incomeTable = new ArrayList<IncomeData>();
+    private Income income = new Income();
+    private IncomeBuisnessActivity business = new IncomeBuisnessActivity();
+    
+    private HashSet<Income> incomeSet = new HashSet();
+    private HashSet<IncomeBuisnessActivity> businessSet = new HashSet();
+    
+    //Fields used to make select menus working
+    
+    private int incomeId;
+    private int branchId;
+    private boolean isIncome = true;
+    
+    //Address Data
+    
+    private int regionId;
+
+    public int getRegionId() {
+        return regionId;
+    }
+
+    public void setRegionId(int regionId) {
+        this.regionId = regionId;
+    }
+    private Address address = new Address();
+    private HashSet<Address> addressSet = new HashSet();
     
     public CreateClientMB()
     {
@@ -118,6 +168,106 @@ public class CreateClientMB implements Serializable
     {
         return "/consultant/consultantCreateAccount?faces-redirect=true";   
     }
+    
+    public void cleanDialog()
+    {
+       
+        income = new Income();
+        branchId = 0;
+        incomeId = 0;
+        
+        System.out.println("Wyczyściło");
+    }
+    
+    public void addIncome()
+    {
+                EmploymentType et = null; 
+                for (EmploymentType i : dictionaryMB.getIncome())
+                {
+                    if (i.getIdEmploymentType() == incomeId)
+                    {
+                        et = i;
+                        break;
+                    }
+                }
+                
+                Branch b = null;
+ 
+                for (Branch i : dictionaryMB.getBranch())
+                {
+                    if (i.getIdBranch() == branchId)
+                    {
+                        b = i;
+                        break;
+                    }
+                }
+                if (isIncome)
+                {
+                    income.setBranch(b);
+                    income.setEmploymentType(et);
+                    income.setClient(loginMB.getClient());
+                    incomeTable.add(new IncomeData(et.getName(), b.getName(), income.getMonthlyNetto().doubleValue()));   
+                    incomeSet.add(income);
+                }
+                else
+                {
+                    business.setBranch(b);
+                    business.setEmploymentType(et);
+                    business.setClient(loginMB.getClient());
+                    incomeTable.add(new IncomeData(et.getName(), b.getName(), business.getIncomeCurrentYearNetto().doubleValue()));         
+                    businessSet.add(business);
+                }
+    }
+    
+    public String saveAndRedirect()
+    {
+        System.out.println("Dalej");
+        ClientDAO clientDao = new ClientDAO();
+        
+        Client client = loginMB.getClient();
+        
+        for (Region i : dictionaryMB.getRegion())
+        {
+            if (i.getIdRegion() == regionId)
+            {
+                address.setRegion(i);
+                break;
+            }
+        }
+        
+        address.setClient(client);
+        addressSet.add(address);
+        
+        client.setAddresses(addressSet);
+        
+        client.setName(name);
+        client.setFamilyName(familyName);
+        client.setLastName(surname);
+        client.setBirthDate(birthDate);
+        client.setPesel(pesel);
+          
+        client.setIncomes(incomeSet);
+        client.setIncomeBuisnessActivities(businessSet);
+        
+        clientDao.update(client);
+        
+        return "/client/clientFinancialNeeds?faces-redirect=true";
+    }
+
+ 
+    
+    public void toIncome()
+    {
+        setIsIncome(true);
+        System.out.println(isIncome);
+    }
+    
+    public void toBusinessActivity()
+    {
+        setIsIncome(false);
+        System.out.println(isIncome);
+    }
+
 
     public int getLoginNumber() {
         return loginNumber;
@@ -173,6 +323,134 @@ public class CreateClientMB implements Serializable
 
     public void setLoginMB(LoginMB loginMB) {
         this.loginMB = loginMB;
+    }
+
+    public DictionaryMB getDictionaryMB() {
+        return dictionaryMB;
+    }
+
+    public void setDictionaryMB(DictionaryMB dictionaryMB) {
+        this.dictionaryMB = dictionaryMB;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(int maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+
+    public int getEducation() {
+        return education;
+    }
+
+    public void setEducation(int education) {
+        this.education = education;
+    }
+
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(Boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public int getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(int birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public ArrayList<IncomeData> getIncomeTable() {
+        return incomeTable;
+    }
+
+    public void setIncomeTable(ArrayList<IncomeData> incomeTable) {
+        this.incomeTable = incomeTable;
+    }
+
+    public Income getIncome() {
+        return income;
+    }
+
+    public void setIncome(Income income) {
+        this.income = income;
+    }
+
+    public IncomeBuisnessActivity getBusiness() {
+        return business;
+    }
+
+    public void setBusiness(IncomeBuisnessActivity business) {
+        this.business = business;
+    }
+
+    public int getIncomeId() {
+        return incomeId;
+    }
+
+    public void setIncomeId(int incomeId) {
+        this.incomeId = incomeId;
+    }
+
+    public int getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(int branchId) {
+        this.branchId = branchId;
+    }
+
+    public boolean isIsIncome() {
+        return isIncome;
+    }
+
+    public void setIsIncome(boolean isIncome) {
+        this.isIncome = isIncome;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
     
            
