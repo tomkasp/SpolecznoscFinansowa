8aa31b4a981f1d23716199d3f88830e818c3a60a
Doradca <Doradca@192.168.0.38>
2013-08-01 12:07:56 +0200

    remote

---
+++ src/main/java/com/efsf/sf/sql/dao/ClientCaseDAO.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.efsf.sf.sql.dao;
+
+import com.efsf.sf.sql.entity.ClientCase;
+import com.efsf.sf.sql.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author XaI
+ */
+public class ClientCaseDAO 
+{
+    public List last5Cases()
+    {
+         List<ClientCase> list;
+         Session session = HibernateUtil.getSessionFactory().openSession();
+         session.beginTransaction();
+         
+         Query q = session.createQuery("from ClientCase order by BeginDate");
+         
+         q.setMaxResults(5);
+         
+         list = q.list();
+         
+         return list;
+    }
+    
+    
+}
