1edc58d23fbfbfe1b9c2a32cf18e0d9a76c10f4c
WR1EI1 <WR1EI1@192.168.0.4>
2013-08-02 08:36:43 +0200

    uii

--- src/main/java/com/efsf/sf/sql/dao/SubscriptionDAO.java
+++ src/main/java/com/efsf/sf/sql/dao/SubscriptionDAO.java
@@ -3,6 +3,7 @@ package com.efsf.sf.sql.dao;
 import com.efsf.sf.sql.entity.Subscription;
 import com.efsf.sf.sql.util.HibernateUtil;
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 /**
@@ -39,6 +40,28 @@ public class SubscriptionDAO {
         session.close();
         }
         
+    }
+    
+     public Subscription loadFkConsultant(Integer fkConsuntant) {
+         
+        Subscription subscription = null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try{
+        session.beginTransaction();
+        
+        Query q = null;
+        q = session.createQuery("FROM Subscription WHERE fk_consultant = :id");
+        q.setParameter("id", fkConsuntant);
+        subscription=(Subscription) q.list().get(0);
+                
+        session.getTransaction().commit();
+        }
+        catch(HibernateException e)
+        {}
+        finally{
+        session.close();
+        }
+        return subscription;
     }
    
 }
