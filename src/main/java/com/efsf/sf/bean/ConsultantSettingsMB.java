1aa118f24ecc5ae43e006a931f519069fe328eff
WR1EI1 <WR1EI1@192.168.0.4>
2013-08-02 11:47:38 +0200

    uii

--- src/main/java/com/efsf/sf/bean/ConsultantSettingsMB.java
+++ src/main/java/com/efsf/sf/bean/ConsultantSettingsMB.java
@@ -8,9 +8,11 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -19,14 +21,16 @@ import javax.faces.validator.ValidatorException;
 /**
  * @author WR1EI1
  */
+
 @ManagedBean
-@SessionScoped
+@RequestScoped
 public class ConsultantSettingsMB implements Serializable {
     private static final long serialVersionUID = 1L;
     //Update Settings
+    
     @ManagedProperty(value="#{loginMB.consultant.idConsultant}")
     private Integer idConsultant;
-    
+   
     private Consultant consultant; 
     private String confirmPassword = new String();
     
@@ -39,11 +43,63 @@ public class ConsultantSettingsMB implements Serializable {
     private Integer idInvoiceRegion;
     private Integer idSubscriptionType;
     
-    public ConsultantSettingsMB() {
-        ConsultantDAO cdao=new ConsultantDAO();
-        consultant=cdao.readConsultantForSettings(idConsultant);
+    public ConsultantSettingsMB() { 
     }
-
+ 
+    @PostConstruct
+    private void loadConsultant() {
+        ConsultantDAO cdao=new ConsultantDAO();
+        consultant=cdao.readConsultantForSettings( idConsultant );
+        //System.out.println("IDEK: "+consultant.getWorkingPlace().getIdWorkingPlace());
+        idWorkingPlace=consultant.getWorkingPlace().getIdWorkingPlace();
+        //idSelectedBankList = consultant.getRegion().
+               
+        Iterator<Institution> it=consultant.getInstitutions().iterator();
+        while(it.hasNext())
+        {
+            Institution i=it.next();
+            if(i.getType()==0)
+            { idSelectedBankList.add( i.getIdInstitution() ); }
+            if(i.getType()==1)
+            { idSelectedInstitutionList.add( i.getIdInstitution() ); }       
+        }
+        
+        Iterator<ProductType> it2=consultant.getProductTypes().iterator();
+        while(it2.hasNext())
+        {
+            ProductType pt=it2.next();
+            idProductTypes.add( pt.getIdProductType() );          
+        }
+        
+        
+        idWorkingPlace=consultant.getWorkingPlace().getIdWorkingPlace();
+        
+        idRegion=consultant.getRegion().getIdRegion();
+        
+        Iterator<Address> it3=consultant.getAddresses().iterator();
+        while(it3.hasNext())
+        {
+            Address a=it3.next();
+            if(a.getType()==1)
+            { idMainRegion=a.getIdAddress(); }
+            if(a.getType()==2)
+            { idInvoiceRegion=a.getIdAddress(); }   
+        }
+        
+        Iterator<Subscription> it4=consultant.getSubscriptions().iterator();
+        while(it4.hasNext())
+        {
+            Subscription s=it4.next();
+            if(!it4.hasNext())//ZAWSZE ZWRACA OSTATNI ABONAMENT
+            {
+                idSubscriptionType=s.getIdSubscription();
+            }
+        }
+        
+        
+        
+    }
+    
     public String updateSettings() {
 
         DictionaryMB dictionaryMB = new DictionaryMB();
@@ -124,7 +180,7 @@ public class ConsultantSettingsMB implements Serializable {
     public void setIdConsultant(Integer idConsultant) {
         this.idConsultant = idConsultant;
     }
-
+    
     public Consultant getConsultant() {
         return consultant;
     }
