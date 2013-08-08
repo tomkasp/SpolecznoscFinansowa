ce7de5e6c44627fc0fdebb481e8d927e603f254e
WR1EI1 <WR1EI1@192.168.0.4>
2013-08-08 13:11:15 +0200

    uii

--- src/main/java/com/efsf/sf/util/validator/LoginValidator.java
+++ src/main/java/com/efsf/sf/util/validator/LoginValidator.java
@@ -2,14 +2,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.efsf.sf.util;
+package com.efsf.sf.util.validator;
 
 import com.efsf.sf.sql.dao.UserDAO;
+import com.efsf.sf.util.Security;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ResourceBundle;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
@@ -19,18 +21,38 @@ import javax.faces.validator.ValidatorException;
  * @author WR1EI1
  */
 
-@FacesValidator("com.efsf.sf.util.EmailValidator")
-public class EmailValidator implements Validator {
+@FacesValidator("LoginValidator")
+public class LoginValidator implements Validator {
     
    @Override
    public void validate(FacesContext facesContext, UIComponent component, Object value) throws ValidatorException {
       
+       String email = value.toString();
+ 
+	  UIInput uiInputConfirmPassword = (UIInput) component.getAttributes()
+		.get("password");
+	  String password = uiInputConfirmPassword.getSubmittedValue()
+		.toString();
+       
+      password=Security.sha1(password);
+       
       UserDAO udao=new UserDAO();
-      Boolean ifEmailExist=udao.ifEmailExist(value.toString());
-      if(!ifEmailExist){
+      
+      int loginStatus=udao.checkLogin(email, password);
+      
+      
+      if(loginStatus==-1){
          FacesContext context = FacesContext.getCurrentInstance();
          ResourceBundle bundle = context.getApplication().getResourceBundle(context, "msg");
          FacesMessage msg =  new FacesMessage(bundle.getString("failed1"),bundle.getString("failed1"));
+         msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+         throw new ValidatorException(msg);
+      }
+      
+      if(loginStatus==0){
+         FacesContext context = FacesContext.getCurrentInstance();
+         ResourceBundle bundle = context.getApplication().getResourceBundle(context, "msg");
+         FacesMessage msg =  new FacesMessage(bundle.getString("failed2"),bundle.getString("failed2"));
          msg.setSeverity(FacesMessage.SEVERITY_ERROR);
          throw new ValidatorException(msg);
       }
