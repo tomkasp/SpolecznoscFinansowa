eb8d5f82f9c3e8010dc28b4f83dbe292bdee9b60
WR1EI1 <WR1EI1@192.168.0.4>
2013-08-09 11:50:51 +0200

    uii

--- src/main/java/com/efsf/sf/bean/client/ClientCaseMB.java
+++ src/main/java/com/efsf/sf/bean/client/ClientCaseMB.java
@@ -18,7 +18,6 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import org.primefaces.context.RequestContext;
 
 @ManagedBean
 @SessionScoped
