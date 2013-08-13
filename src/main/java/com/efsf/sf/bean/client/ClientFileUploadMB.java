abe73011989acb4041e08aa1f2dfe9420c0b380e
WR1EI1 <WR1EI1@192.168.0.4>
2013-08-13 12:30:03 +0200

    uii

--- src/main/java/com/efsf/sf/bean/client/ClientFileUploadMB.java
+++ src/main/java/com/efsf/sf/bean/client/ClientFileUploadMB.java
@@ -1,7 +1,10 @@
 package com.efsf.sf.bean.client;
 
+import com.efsf.sf.bean.LoginMB;
+import com.efsf.sf.util.uploader.local.FileUploadController;
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import org.primefaces.model.UploadedFile;
 
@@ -14,6 +17,10 @@ import org.primefaces.model.UploadedFile;
 public class ClientFileUploadMB implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    
+    @ManagedProperty(value = "#{loginMB}")
+    private LoginMB loginMB;
+    
     private UploadedFile[] uploadedFiles = new UploadedFile[8];
     
     public ClientFileUploadMB() {
@@ -25,6 +32,17 @@ public class ClientFileUploadMB implements Serializable {
 
     public void setUploadedFiles(UploadedFile[] uploadedFiles) {
         this.uploadedFiles = uploadedFiles;
-    }   
+    }  
+    
+    public String save(){
+        
+        //ZALACZNIKI:
+            String[] lokalizacje = new String[8];
+            FileUploadController fuc = new FileUploadController();
+            
+               lokalizacje[0] = fuc.upload(uploadedFiles[0], loginMB.getIdUser() , "dowodTozsamosci");        
+            
+        return "";
+    }
     
 }
