43feef29d5bcc8e6ac99d8be69bb5d7b5871a09e
Doradca <Doradca@192.168.0.38>
2013-08-22 11:12:40 +0200

    CONSTANTS

---
+++ src/main/java/com/efsf/sf/util/Settings.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.efsf.sf.util;
+
+/**
+ *
+ * @author XaI
+ */
+public class Settings 
+{
+    // USER TYPES!! 
+    public static final Integer ADMIN_UNVERIFIED = 10;
+    public static final Integer ADMIN_ACTIVE = 11;
+    public static final Integer ADMIN_INACTIVE = 12;
+    public static final Integer CONSULTANT_UNVERIFIED = 20;
+    public static final Integer CONSULTANT_ACTIVE = 21;
+    public static final Integer CONSULTANT_INACTIVE = 22;
+    public static final Integer CLIENT_UNVERIFIED = 30;
+    public static final Integer CLIENT_ACTIVE = 31;
+    public static final Integer CLIENT_INACTIVE = 32;
+    
+    public static final Integer LOGGED_OUT = -1;
+    
+    
+}
