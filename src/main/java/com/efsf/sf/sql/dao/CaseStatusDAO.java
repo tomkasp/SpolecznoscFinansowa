8bb08606708211c0db2fdebb81aab183f804ad3e
michal <michal.oles@gmail.com>
2013-08-30 11:36:34 +0200

    Finally

--- src/main/java/com/efsf/sf/sql/dao/CaseStatusDAO.java
+++ src/main/java/com/efsf/sf/sql/dao/CaseStatusDAO.java
@@ -6,37 +6,37 @@ import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
-
 public class CaseStatusDAO {
-    
-    public List caseStatusList(){
+
+    public List caseStatusList() {
         List<CaseStatus> lista;
         Session session = HibernateUtil.SESSION_FACTORY.openSession();
-        session.beginTransaction();
-        lista = session.createQuery("from CaseStatus").list();
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            lista = session.createQuery("from CaseStatus").list();
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
         return lista;
     }
-    
-     public CaseStatus read(int id) {
-       
+
+    public CaseStatus read(int id) {
+
         Session session = HibernateUtil.SESSION_FACTORY.openSession();
-        CaseStatus caseStatus=null;
-        try
-        {
-        session.beginTransaction().begin();
-        Query q;
-        q = session.createQuery("FROM CaseStatus WHERE id_caseStatus = :id");
-        q.setParameter("id", id);
-        caseStatus=(CaseStatus) q.list().get(0);
-        session.getTransaction().commit();
-        
-        }finally{  
-        session.close();
+        CaseStatus caseStatus = null;
+        try {
+            session.beginTransaction().begin();
+            Query q;
+            q = session.createQuery("FROM CaseStatus WHERE id_caseStatus = :id");
+            q.setParameter("id", id);
+            caseStatus = (CaseStatus) q.list().get(0);
+            session.getTransaction().commit();
+
+        } finally {
+            session.close();
         }
 
         return caseStatus;
     }
-
 }
