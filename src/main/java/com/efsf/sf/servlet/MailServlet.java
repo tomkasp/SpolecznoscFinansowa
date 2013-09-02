8c1135d6adcc43a1455daf127a8830bc473bd482
WR1EI1 <WR1EI1@192.168.0.4>
2013-09-02 14:33:44 +0200

    uii

---
+++ src/main/java/com/efsf/sf/servlet/MailServlet.java
@@ -0,0 +1,17 @@
+package com.efsf.sf.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class MailServlet extends HttpServlet{
+ 
+    //Read payment Status
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response){
+      
+    }
+
+    
+}
