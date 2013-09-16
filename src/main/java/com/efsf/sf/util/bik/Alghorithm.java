614442aa922bd434256597d1c237e6d01b239dab
michal <michal.oles@gmail.com>
2013-09-16 14:33:18 +0200

    refactor

--- src/main/java/com/efsf/sf/util/bik/Alghorithm.java
+++ src/main/java/com/efsf/sf/util/bik/Alghorithm.java
@@ -2,9 +2,9 @@ package com.efsf.sf.util.bik;
 
 import com.efsf.sf.sql.dao.GenericDao;
 import com.efsf.sf.sql.entity.Bik;
-import com.efsf.sf.sql.entity.BikHistoriaRachunku;
-import com.efsf.sf.sql.entity.BikRachunek;
-import com.efsf.sf.sql.entity.BikZapytanie;
+import com.efsf.sf.sql.entity.BikAccountHistory;
+import com.efsf.sf.sql.entity.BikAccount;
+import com.efsf.sf.sql.entity.BikQuestion;
 import com.efsf.sf.sql.entity.Client;
 import com.efsf.sf.sql.entity.RequiredDocuments;
 import java.io.File;
@@ -145,55 +145,55 @@ public class Alghorithm extends Thread{
             
         //Bik główne dane
         Bik bik = new Bik();
-        bik.setKlasa((String) result.get(RegexBase.KLASYFIKACJA_BIK.getName()));
-        bik.setOcenaPunktowa((String) result.get(RegexBase.OCENA_PUNKTOWA.getName()));
+        bik.setBikClass((String) result.get(RegexBase.KLASYFIKACJA_BIK.getName()));
+        bik.setBikRank((String) result.get(RegexBase.OCENA_PUNKTOWA.getName()));
         bik.setPesel((String) result.get(RegexBase.PESEL.getName()));
-        bik.setRzZaleglosc030dni((String) result.get(RegexBase.RACH_ZAMK_ZALEG_0_30.getName()));
-        bik.setRzUmorzone((String) result.get(RegexBase.RACH_ZAMK_UMORZONY.getName()));
-        bik.setRzOdzyskane((String) result.get(RegexBase.RACH_ZAMK_ODZYSKANY.getName()));
-        bik.setRoZaleglosc030dni((String) result.get(RegexBase.RACH_OTW_ZALEG_0_30.getName()));
-        bik.setRoZaleglosc3190dni((String) result.get(RegexBase.RACH_OTW_ZALEG_31_90.getName()));
-        bik.setRoZaleglosc91180dni((String) result.get(RegexBase.RACH_OTW_ZALEG_91_180.getName()));
-        bik.setRoZaleglosc180dni((String) result.get(RegexBase.RACH_OTW_ZALEG_180_WIECEJ.getName()));
-        bik.setRoEgzekucja((String) result.get(RegexBase.RACH_OTW_EGZEKUCJA.getName()));
-        bik.setRoWindykacja((String) result.get(RegexBase.RACH_OTW_WINDYKACJA.getName()));
-        bik.setRachunkiKwestionowane((String) result.get(RegexBase.RACHUNKI_KWESTIONOWANE.getName()));
+        bik.setClosedAccountsArrear030days((String) result.get(RegexBase.RACH_ZAMK_ZALEG_0_30.getName()));
+        bik.setClosedAccountsExtinguished((String) result.get(RegexBase.RACH_ZAMK_UMORZONY.getName()));
+        bik.setClosedAccountsRegained((String) result.get(RegexBase.RACH_ZAMK_ODZYSKANY.getName()));
+        bik.setOpenAccountsArrear030days((String) result.get(RegexBase.RACH_OTW_ZALEG_0_30.getName()));
+        bik.setOpenAccountsArrear3190days((String) result.get(RegexBase.RACH_OTW_ZALEG_31_90.getName()));
+        bik.setOpenAccountsArrear91180days((String) result.get(RegexBase.RACH_OTW_ZALEG_91_180.getName()));
+        bik.setOpenAccountsArrear180days((String) result.get(RegexBase.RACH_OTW_ZALEG_180_WIECEJ.getName()));
+        bik.setOpenAccountsExecution((String) result.get(RegexBase.RACH_OTW_EGZEKUCJA.getName()));
+        bik.setOpenAccountsCollection((String) result.get(RegexBase.RACH_OTW_WINDYKACJA.getName()));
+        bik.setImpeachedAccounts((String) result.get(RegexBase.RACHUNKI_KWESTIONOWANE.getName()));
         bik.setClient(client);
         
         GenericDao<Bik> dao = new GenericDao(Bik.class);
         dao.save(bik);
 
         //Rachunki
-        GenericDao<BikRachunek> dao2 = new GenericDao(BikRachunek.class);
+        GenericDao<BikAccount> dao2 = new GenericDao(BikAccount.class);
         ArrayList<String> kwoty = (ArrayList<String>) result.get(RegexBase.R_KWOTA.getName());
         if (kwoty != null) {
 
             for (int i = 0; i < kwoty.size(); i++) {
-             BikRachunek rachunek=new BikRachunek();
-             rachunek.setWaluta(((ArrayList<String>) result.get(RegexBase.R_WALUTA.getName())).get(i));
-             rachunek.setDataPowstaniaRelacji(((ArrayList<String>) result.get(RegexBase.R_DATA_RELACJI.getName())).get(i));
-             rachunek.setTypTransakcji(((ArrayList<String>) result.get(RegexBase.R_TYP_TRANSAKCJI.getName())).get(i));
-             rachunek.setRelacjaKlienta(((ArrayList<String>) result.get(RegexBase.R_RELACJA.getName())).get(i));
-             rachunek.setKwotaZKosztemOds1(((ArrayList<String>) result.get(RegexBase.R_KWOTA.getName())).get(i));
+             BikAccount rachunek=new BikAccount();
+             rachunek.setCurrency(((ArrayList<String>) result.get(RegexBase.R_WALUTA.getName())).get(i));
+             rachunek.setCreateRelationDate(((ArrayList<String>) result.get(RegexBase.R_DATA_RELACJI.getName())).get(i));
+             rachunek.setTransactionType(((ArrayList<String>) result.get(RegexBase.R_TYP_TRANSAKCJI.getName())).get(i));
+             rachunek.setClientRelation(((ArrayList<String>) result.get(RegexBase.R_RELACJA.getName())).get(i));
+             rachunek.setAmountWithInterestExpense1(((ArrayList<String>) result.get(RegexBase.R_KWOTA.getName())).get(i));
              rachunek.setBank(((ArrayList<String>) result.get(RegexBase.R_BANK.getName())).get(i));
              rachunek.setBik(bik);
              dao2.save(rachunek);
              
                 //Zapisz historie rachunku
-                GenericDao<BikHistoriaRachunku> dao3 = new GenericDao(BikHistoriaRachunku.class);
+                GenericDao<BikAccountHistory> dao3 = new GenericDao(BikAccountHistory.class);
                 
                 for(int j=0; j<tables.get(i).data.size();j++){
-                    BikHistoriaRachunku historia=new BikHistoriaRachunku();
-                    historia.setRachunek(rachunek);
-                    historia.setNumer(tables.get(i).data.get(j)[0]);
-                    historia.setData(tables.get(i).data.get(j)[1]);
+                    BikAccountHistory historia=new BikAccountHistory();
+                    historia.setAccount(rachunek);
+                    historia.setNumber(tables.get(i).data.get(j)[0]);
+                    historia.setCreatedDate(tables.get(i).data.get(j)[1]);
                     historia.setStatus(tables.get(i).data.get(j)[2]);
-                    historia.setKwotaDoSplaty(tables.get(i).data.get(j)[3]);
-                    historia.setSaldoNaleznosci(tables.get(i).data.get(j)[04]);
-                    historia.setOpoznienie(tables.get(i).data.get(j)[5]);
-                    historia.setZaleglePlatnosci(tables.get(i).data.get(j)[6]);
-                    historia.setSaldoLimitu(tables.get(i).data.get(j)[7]);
-                    historia.setWaluta(tables.get(i).data.get(j)[8]);
+                    historia.setAmount(tables.get(i).data.get(j)[3]);
+                    historia.setOverduePayments(tables.get(i).data.get(j)[04]);
+                    historia.setPaymentDelay(tables.get(i).data.get(j)[5]);
+                    historia.setOverduePayments(tables.get(i).data.get(j)[6]);
+                    historia.setLimitBalance(tables.get(i).data.get(j)[7]);
+                    historia.setCurrency(tables.get(i).data.get(j)[8]);
                     dao3.save(historia);
                 }
                 
@@ -203,16 +203,16 @@ public class Alghorithm extends Thread{
         
         
         //Zapytania
-        GenericDao<BikZapytanie> dao3 = new GenericDao(BikZapytanie.class);
+        GenericDao<BikQuestion> dao3 = new GenericDao(BikQuestion.class);
         ArrayList<String> zapytania = (ArrayList<String>) result.get(RegexBase.Z_KWOTA.getName());
         if (zapytania != null) {
 
             for (int i = 0; i < zapytania.size(); i++) {
-             BikZapytanie zapytanie=new BikZapytanie();
-             zapytanie.setData(((ArrayList<String>) result.get(RegexBase.Z_DATA.getName())).get(i));
-             zapytanie.setKwota(((ArrayList<String>) result.get(RegexBase.Z_KWOTA.getName())).get(i));
-             zapytanie.setPowod(((ArrayList<String>) result.get(RegexBase.Z_POWOD.getName())).get(i));
-             zapytanie.setTyp(((ArrayList<String>) result.get(RegexBase.Z_TYP.getName())).get(i));
+             BikQuestion zapytanie=new BikQuestion();
+             zapytanie.setQuestionDate(((ArrayList<String>) result.get(RegexBase.Z_DATA.getName())).get(i));
+             zapytanie.setAmount(((ArrayList<String>) result.get(RegexBase.Z_KWOTA.getName())).get(i));
+             zapytanie.setReason(((ArrayList<String>) result.get(RegexBase.Z_POWOD.getName())).get(i));
+             zapytanie.setType(((ArrayList<String>) result.get(RegexBase.Z_TYP.getName())).get(i));
              zapytanie.setBik(bik);
              dao3.save(zapytanie);
             }
