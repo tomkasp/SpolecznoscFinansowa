c5e4c4ce74e084bfe1d64514b32d01965af54c14
michal <michal.oles@gmail.com>
2013-10-11 09:56:54 +0200

    invoice

--- src/main/java/com/efsf/sf/bean/consultant/ReportsMB.java
+++ src/main/java/com/efsf/sf/bean/consultant/ReportsMB.java
@@ -2,10 +2,15 @@ package com.efsf.sf.bean.consultant;
 
 import com.efsf.sf.bean.LoginMB;
 import com.efsf.sf.sql.dao.AddressDAO;
-import com.efsf.sf.sql.dao.ConsultantDAO;
+import com.efsf.sf.sql.dao.GenericDao;
 import com.efsf.sf.sql.entity.Address;
+import com.efsf.sf.sql.entity.SubscriptionType;
+import com.efsf.sf.util.NumberSpeaker;
+import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import javax.faces.bean.ManagedBean;
@@ -13,14 +18,17 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRParameter;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.SimpleFileResolver;
 
 @ManagedBean
 @RequestScoped
@@ -37,12 +45,21 @@ public class ReportsMB implements Serializable{
     public void setLoginMB(LoginMB loginMB) {
         this.loginMB = loginMB;
     }
+    
+    
+    private String getReportImagePath() throws MalformedURLException{
+            HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+            URL url = new URL(request.getRequestURL().toString());
+            return url.getProtocol()+"://"+url.getHost()+":"+url.getPort()+"/"+request.getContextPath()
+                +"/reports/";
+    }
 
     public void generateInvoice() throws JRException, IOException{
-        
+        GenericDao<SubscriptionType> dao=new GenericDao(SubscriptionType.class);
         Map<String, Object> params=new HashMap<String, Object>();
         params.put("consultant", loginMB.getConsultant());
         params.put("address", getInvoiceAddress());
+        params.put("sub", dao.getById(1));
         export("invoice.jrxml", params, new JREmptyDataSource());
     }
     
@@ -61,6 +78,12 @@ public class ReportsMB implements Serializable{
     private void export(String template, Map<String, Object> params, JRDataSource dataSource) throws IOException, JRException {
         JasperReport report;
 
+        if (params != null) {
+            String reportsDirPath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/reports/");
+            File reportsDir = new File(reportsDirPath);
+            params.put(JRParameter.REPORT_FILE_RESOLVER, new SimpleFileResolver(reportsDir));
+        }
+        
         ExternalContext ctx=FacesContext.getCurrentInstance().getExternalContext();
         ctx.setResponseContentType("application/pdf");
         try {
@@ -73,6 +96,12 @@ public class ReportsMB implements Serializable{
         }
 
         FacesContext.getCurrentInstance().responseComplete();
+    }
+    
+    public void test(){
+        
+          System.out.println(125.45+" | "+NumberSpeaker.speakNumber(125.45));
+
     }
     
     
