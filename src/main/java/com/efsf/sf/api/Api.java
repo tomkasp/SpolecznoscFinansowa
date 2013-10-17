3517745cda192924daf73ba48eb8eb1683494d37
Doradca <Doradca@192.168.0.38>
2013-10-17 09:34:18 +0200

    Invoice requirements

--- src/main/java/com/efsf/sf/api/Api.java
+++ src/main/java/com/efsf/sf/api/Api.java
@@ -2,16 +2,32 @@ package com.efsf.sf.api;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/api")
 public class Api{
     
-    @GET
-    @Path("/users")
-    public Response getUsers() {
-        return Response.status(200).entity("jest :D").build();
-        
-    }    
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String sayPlainTextHello() {
+    return "Hello Jersey";
+  }
+
+  // This method is called if XML is request
+  @GET
+  @Produces(MediaType.TEXT_XML)
+  public String sayXMLHello() {
+    return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+  }
+
+  // This method is called if HTML is request
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  public String sayHtmlHello() {
+    return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+        + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+  }  
     
 }
