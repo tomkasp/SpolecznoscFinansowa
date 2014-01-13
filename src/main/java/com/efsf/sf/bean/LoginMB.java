ccb420f05d802cdf4ee73a10809f56f88ace560c
michal <michal.oles@gmail.com>
2014-01-13 08:15:15 +0100

    fix

--- src/main/java/com/efsf/sf/bean/LoginMB.java
+++ src/main/java/com/efsf/sf/bean/LoginMB.java
@@ -23,7 +23,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.joda.time.LocalDate;
 
 @ManagedBean
 @SessionScoped
