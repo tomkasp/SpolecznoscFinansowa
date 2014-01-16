ec4367e76c73ce90642a217509faa369788c6eac
michal <michal.oles@gmail.com>
2014-01-16 09:05:10 +0100

    ...

---
+++ src/main/java/com/efsf/sf/bean/client/CsvMB.java
@@ -0,0 +1,13 @@
+package com.efsf.sf.bean.client;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class CsvMB implements Serializable{
+
+    
+    
+}
